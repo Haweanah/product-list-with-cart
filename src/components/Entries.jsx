5b9d67e8af64abd83057ec3f24e4b395f8673eb4
Haweanah <ahauwa48@yahoo.com>
2026-02-27 13:02:34 +0100

    correct image paths

--- src/components/Entries.jsx
+++ src/components/Entries.jsx
@@ -9,7 +9,7 @@ export default function Entries(props) {
 
       <div className="add-to-cart">
         <button onClick={() => props.increase(props.id)}>
-          <img src="/images/images/icon-add-to-cart.svg" alt="" />
+          <img src="/assets/images/icon-add-to-cart.svg" alt="" />
           Add to Cart
         </button>
       </div>
@@ -32,7 +32,7 @@ export default function Entries(props) {
         <button 
         className="decrease-btn"
         onClick={() => props.decrease(props.id)}>
-          <img src="/images/images/icon-decrement-quantity.svg" alt="" />
+          <img src="/assets/images/icon-decrement-quantity.svg" alt="" />
         </button>
 
         <div className="">{props.quantity}</div>
@@ -40,7 +40,7 @@ export default function Entries(props) {
         <button 
         className="add-btn"
         onClick={() => props.increase(props.id)}>
-          <img src="/images/icon-increment-quantity.svg" alt="" />
+          <img src="/assets/images/icon-increment-quantity.svg" alt="" />
         </button>
       </button>
 
